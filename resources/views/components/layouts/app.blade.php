bddfce8a408579dbf1a10c12baada63fee5b7ce1
KokoFibo <kokonacci@gmail.com>
2024-03-05 20:58:11 +0700

    tambah landing page 1

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <style>
         body {
