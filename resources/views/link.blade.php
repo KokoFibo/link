aadcedc7e95c42b8e6e3a1af441af59072ffb442
KokoFibo <kokonacci@gmail.com>
2024-03-06 21:42:23 +0700

    tambah vcf 4

--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -243,6 +243,10 @@
         @else
             <p>File Not Found</p>
         @endif
+
+        <p class="text-sm text-gray-600 text-center py-5 ">Copyright <i class="fa-regular fa-copyright"></i> Accel365
+            2024
+        </p>
     </div>
 
 @endsection
