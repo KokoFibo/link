685622d0e4ee6460a22dff7725cd5acb2e696f4f
KokoFibo <kokonacci@gmail.com>
2024-03-08 11:01:50 +0700

    rubah code

---
+++ app/helpers.php
@@ -0,0 +1,6 @@
+<?php
+
+function create_code()
+{
+    return fake()->word();
+}
--- resources/views/registration-form.blade.php
+++ resources/views/registration-form.blade.php
@@ -149,6 +149,20 @@
                          class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Youtube</label>
                  </div>
              </div>
+             {{-- Code --}}
+             <div class="relative z-0 w-full mb-5 group">
+                 <input wire:model.live="code" type="text" name="floating_code" id="floating_code"
+                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                     placeholder=" " />
+                 <label for="floating_code"
+                     class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Code</label>
+                 <span class="text-red-500 text-xs">
+                     @error('code')
+                         {{ $message }}
+                     @enderror
+                 </span>
+             </div>
+             {{-- upload Photo --}}
              <div class="border-2 rounded-xl p-5 mt-5">
                  @if ($photo)
                      <img src="{{ $photo->temporaryUrl() }}">
