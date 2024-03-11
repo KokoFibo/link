6585fca5d78ccaafc740ce222f2ae4672f217c44
KokoFibo <kokonacci@gmail.com>
2024-03-11 14:13:18 +0700

    tambah alamat 8

--- resources/views/livewire/update-data.blade.php
+++ resources/views/livewire/update-data.blade.php
@@ -6,7 +6,36 @@
             <h1 class="text-3xl font-semibold text-center mt-5">Update Data</h1>
             @include('registration-form-update-for-user')
         </main>
+        <script>
+            $(document).ready(function() {
+                toastr.options = {
+                    progressBar: true,
+                    timeOut: "2000",
+                    progressBar: true,
+                    positionClass: "toast-top-right",
+                    closeButton: true,
+                    preventDuplicates: true,
+                    showEasing: "swing",
+                    hideEasing: "linear",
+                    showMethod: "fadeIn",
+                    hideMethod: "fadeOut",
+                };
+                window.addEventListener("success", (event) => {
+                    toastr.success(event.detail.message);
+                });
+                window.addEventListener("warning", (event) => {
+                    toastr.warning(event.detail.message);
+                });
 
+                window.addEventListener("info", (event) => {
+                    toastr.info(event.detail.message);
+                });
+
+                window.addEventListener("error", (event) => {
+                    toastr.error(event.detail.message);
+                });
+            });
+        </script>
     </x-app-layout>
 
 
